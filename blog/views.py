7a0112c481c647e227b6269baa7f4e62324d888e
mageste <mageste01@gmail.com>
2019-08-23 16:10:44 +0000

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 
+def post_test(request):
+    form = PostForm()
+    return render(request, 'blog/teston.html', {'form': form})
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
